a4f4f04c6e7167a06bae6298a23b12c322ff45a1
Astrofive <kevin.strebel@yahoo.com>
2023-12-22 18:49:57 -0600

    Updated remove.js
    Added instructions to the Amazon search page itself

--- remove.js
+++ remove.js
@@ -1,3 +1,4 @@
+// function for clearing
 function removeNonChristmasResults() {
   // Find all div elements with the attribute data-component-type="s-search-result"
   const elements = document.querySelectorAll('div[data-component-type="s-search-result"]');
@@ -10,8 +11,31 @@ function removeNonChristmasResults() {
   });
 }
 
+// listener for key to clear
 document.addEventListener('keydown', function(event) {
   if (event.code === 'KeyD') {
     removeNonChristmasResults();
   }
 });
+
+// Adds instructions to the Amazon search page itself!
+window.onload = function() {
+    // Define the XPath for the target element
+    let xpath = '//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[2]';
+
+    // Use document.evaluate to find the element
+    let targetElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+    // Check if the target element exists to avoid errors
+    if (targetElement) {
+      console.log('Target element found');
+      
+      // Define the HTML for the new div
+        let newDivHTML = '<div style="padding: 16px; margin: 15px 0; background-color: #ededed; border-radius: 15px; color: #343434;">Press D key to filter by items that arrive before Christmas!</div>';
+
+        // Insert the new div after the target element
+        targetElement.insertAdjacentHTML('afterend', newDivHTML);
+    } else {
+        console.log('Target element not found');
+    }
+};
